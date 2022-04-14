a5e21e1616ea7afce6c7a5958c0fc032ded62aee
Vortlan <utkumacit.55@gmail.com>
2022-04-14 18:39:32 -0500

    Clean Blog Projesi 3. Bölüm | Odev 9 eklendi

---
+++ CleanBlogOdev9/app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const ejs = require("ejs");
+
+const Post = require("./models/Post");
+
+const app = express();
+
+mongoose.connect("mongodb://localhost/cleanblog-test-db");
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  const posts = await Post.find({});
+  res.render("index", {
+    posts,
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+app.get("/add_post", (req, res) => {
+  res.render("add_post");
+});
+app.get("/post", (req, res) => {
+  res.render("post");
+});
+
+app.post("/posts", async (req, res) => {
+  Post.create(req.body);
+  res.redirect("/");
+});
+
+const port = 3000; //
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda baslatildi`);
+});
