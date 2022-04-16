6843b3710a8857537f417df286337d5112800a0c
Vortlan <utkumacit.55@gmail.com>
2022-04-16 16:06:17 -0500

    Clean Blog Projesi 5. Bölüm | Odev 11 eklendi

---
+++ CleanBlogOdev11/app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const ejs = require("ejs");
+
+const Post = require("./models/Post");
+const postController = require("./controllers/postControllers");
+const pageController = require("./controllers/pageControllers");
+
+const app = express();
+
+mongoose.connect("mongodb://localhost/cleanblog-test-db");
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(
+  methodOverride("_method", {
+    methods: ["POST", "GET"],
+  })
+);
+
+app.get("/", postController.getAllPosts);
+app.get("/posts/:id", postController.getPost);
+app.post("/posts", postController.createPost);
+app.put("/posts/:id", postController.updatePost);
+app.delete("/posts/:id", postController.deletePost);
+
+app.get("/about", pageController.getAboutPage);
+app.get("/add_post", pageController.getAddPage);
+app.get("/posts/edit/:id", pageController.getEditPage);
+
+const port = 3000; //
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda baslatildi`);
+});
